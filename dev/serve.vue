7e3b4d7a3ff1d0acffc0641d7839279324aadb30
David Royer <droyer01@gmail.com>
2021-08-17 22:38:41 -0400

    chore: initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VWriterSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  VWriterSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <v-editor/>
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VWriterSample } from './v-writer-sample.vue';
+export { default as VEditor } from './VEditor.vue';
